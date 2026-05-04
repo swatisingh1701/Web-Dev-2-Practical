bf2be82123c767eb41fb37cabef4a96a3d71f7bc
Swati Singh <swati98207@gmail.com>
2026-05-04 10:12:21 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ImagesApi from "./components/ImagesApi";
+
+function App() {
+  return (
+    <div>
+      <ImagesApi />
+    </div>
+  );
+}
+
+export default App;
